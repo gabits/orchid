83d08e4e36867a9f0f3581dabacbfa6dda0f177d
Gabriela Valle <gabrieladvmininel@gmail.com>
2017-11-19 23:08:30 +0000

    Trying to link staticfiles to CSS but not finished

--- portfolio/config/settings/base.py
+++ portfolio/config/settings/base.py
@@ -110,3 +110,6 @@ class BaseConfiguration(Configuration):
     # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
     STATIC_URL = '/static/'
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
