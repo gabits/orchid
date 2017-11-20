d5c74580374010497e0b8c9aed696fd5beab2b7f
Gabriela Valle <gabrieladvmininel@gmail.com>
2017-11-20 23:17:37 +0000

    Renaming and deleting some screwed up migrations...

--- portfolio/works/views.py
+++ portfolio/works/views.py
@@ -1,11 +1,11 @@
 from django.views.generic import ListView
 
 from .models.media import Photograph
-from .models.gallery import GallerySection
+from .models.gallery import Gallery
 
 
 class MainGalleryView(ListView):
-    model = GallerySection
+    model = Gallery
     template_name = 'works/gallery_list.html'
 
 
