3720a9218dac5afb4d0b6c68858d31c25116ab60
Gabriela Valle <gabrieladvmininel@gmail.com>
2017-11-23 22:52:35 +0000

    Forgot the wsgi file... but still not working :(

--- portfolio/config/wsgi.py
+++ portfolio/config/wsgi.py
@@ -2,6 +2,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+from .get_configuration import discover_configuration
+
+
+path, cls = discover_configuration()
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", path)
+os.environ.setdefault("DJANGO_CONFIGURATION", cls)
 
 application = get_wsgi_application()
--- portfolio/manage.py
+++ portfolio/manage.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 import os
 import sys
-from .common.scripts.get_configuration import discover_configuration
+from .config.get_configuration import discover_configuration
 
 
 if __name__ == "__main__":
     path, cls = discover_configuration()
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", path)
-    os.environ.setdefault("DJANGO_CONFIGURATION", cls)
+    DJANGO_SETTINGS_MODULE = path
+    DJANGO_CONFIGURATION = cls
 
     # Get environment variables from env file
     try:
