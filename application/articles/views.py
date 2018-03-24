be407cea4d9a793798c151f857bff446b480e884
Gabriela Valle <gabrieladvmininel@gmail.com>
2018-03-24 16:26:21 +0000

    Add new page for adding articles to the website

--- application/articles/views.py
+++ application/articles/views.py
@@ -1,10 +1,17 @@
-from django.views.generic import ListView
+from django.views import generic as django_views
 
+from .forms import ArticleForm
 from .models import Article
 
 
-class ArticleListView(ListView):
+class ArticleListView(django_views.ListView):
     template_name = 'articles/article_list.html'
     model = Article
     queryset = Article.objects.all()
     ordering = ['-created_at']          # TODO: Replace this by published_at once implemented
+
+
+class AddEditArticleView(django_views.FormView):
+    template_name = 'articles/add_or_edit_article.html'
+    model = Article
+    form_class = ArticleForm
